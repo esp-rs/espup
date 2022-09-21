9afc3382502202a00fa2ec2890e499066a37f1a5
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-09-21 09:49:39 +0000

    chore: 🎨 Remove unused emojis

--- src/emoji.rs
+++ src/emoji.rs
@@ -1,11 +1,10 @@
 use console::Emoji;
 
 pub static ERROR: Emoji<'_, '_> = Emoji("⛔ ", "");
-// pub static CHECK: Emoji<'_, '_> = Emoji("✅ ", "");
+pub static CHECK: Emoji<'_, '_> = Emoji("✅ ", "");
 pub static WARN: Emoji<'_, '_> = Emoji("⚠️ ", "");
 pub static WRENCH: Emoji<'_, '_> = Emoji("🔧 ", "");
 pub static DOWNLOAD: Emoji<'_, '_> = Emoji("📥 ", "");
 pub static INFO: Emoji<'_, '_> = Emoji("💡 ", "");
 pub static DISC: Emoji<'_, '_> = Emoji("💽 ", "");
-pub static DIAMOND: Emoji<'_, '_> = Emoji("🔸 ", "");
 pub static DEBUG: Emoji<'_, '_> = Emoji("🐞 ", "");
