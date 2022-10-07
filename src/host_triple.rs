ffc2611d41be7cffb8c63426cca4f35733a1280f
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-10-07 20:44:50 +0200

    refactor: ♻️ Use strum-macros to avoid reimplementing from_str

--- src/host_triple.rs
+++ src/host_triple.rs
@@ -1,10 +1,11 @@
 use crate::emoji;
-use anyhow::Result;
+use anyhow::{bail, Result};
 use guess_host_triple::guess_host_triple;
 use std::str::FromStr;
 use strum::Display;
+use strum_macros::EnumString;
 
-#[derive(Display, Debug, Clone)]
+#[derive(Display, Debug, Clone, EnumString)]
 pub enum HostTriple {
     /// 64-bit Linux
     #[strum(serialize = "x86_64-unknown-linux-gnu")]
@@ -26,30 +27,17 @@ pub enum HostTriple {
     Aarch64AppleDarwin,
 }
 
-impl FromStr for HostTriple {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "x86_64-unknown-linux-gnu" => Ok(HostTriple::X86_64UnknownLinuxGnu),
-            "aarch64-unknown-linux-gnu" => Ok(HostTriple::Aarch64UnknownLinuxGnu),
-            "x86_64-pc-windows-msvc" => Ok(HostTriple::X86_64PcWindowsMsvc),
-            "x86_64-pc-windows-gnu" => Ok(HostTriple::X86_64PcWindowsGnu),
-            "x86_64-apple-darwin" => Ok(HostTriple::X86_64AppleDarwin),
-            "aarch64-apple-darwin" => Ok(HostTriple::Aarch64AppleDarwin),
-            _ => Err(format!(
-                "{} Host triple '{}' is not supported.",
-                emoji::ERROR,
-                s
-            )),
-        }
-    }
-}
-
 /// Parse the host triple if specified, otherwise guess it.
 pub fn get_host_triple(host_triple_arg: Option<String>) -> Result<HostTriple> {
     let host_triple = match host_triple_arg {
-        Some(host_triple) => HostTriple::from_str(&host_triple).unwrap(),
+        Some(host_triple_string) => match FromStr::from_str(&host_triple_string) {
+            Ok(host_triple) => host_triple,
+            Err(_) => bail!(
+                "{} Host triple '{}' is not supported.",
+                emoji::ERROR,
+                host_triple_string
+            ),
+        },
         None => HostTriple::from_str(guess_host_triple().unwrap()).unwrap(),
     };
     Ok(host_triple)
