84cb87205ece73925c3790c4cbe8882c1921583b
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-10-18 12:55:55 +0000

    refactor: ♻️ Bail when rustup is not installed

--- src/toolchain/rust_toolchain.rs
+++ src/toolchain/rust_toolchain.rs
@@ -194,97 +194,24 @@ pub fn get_rustup_home() -> PathBuf {
     PathBuf::from(env::var("RUSTUP_HOME").unwrap_or_else(|_e| get_home_dir() + "/.rustup"))
 }
 
-/// Checks if rustup and the proper nightly version are installed. If they are
-/// not, proceed to install them.
+/// Checks if rustup and the proper nightly version are installed. If rustup is not installed,
+/// it bails. If nigthly version is not installed, proceed to install it.
 pub fn check_rust_installation(nightly_version: &str) -> Result<()> {
     info!("{} Checking existing Rust installation", emoji::WRENCH);
 
-    match cmd!("rustup", "toolchain", "list")
+    if let Ok(child_output) = cmd!("rustup", "toolchain", "list")
         .into_inner()
         .stdout(Stdio::piped())
         .output()
     {
-        Ok(child_output) => {
-            let result = String::from_utf8_lossy(&child_output.stdout);
-            if !result.contains("nightly") {
-                warn!("{} Rust nightly toolchain not found", emoji::WARN);
-                install_rust_nightly(nightly_version)?;
-            }
+        let result = String::from_utf8_lossy(&child_output.stdout);
+        if !result.contains("nightly") {
+            warn!("{} Rust nightly toolchain not found", emoji::WARN);
+            install_rust_nightly(nightly_version)?;
         }
-        Err(e) => {
-            if let std::io::ErrorKind::NotFound = e.kind() {
-                warn!("{} rustup was not found.", emoji::WARN);
-                install_rustup(nightly_version)?;
-            } else {
-                bail!("{} Error: {}", emoji::ERROR, e);
-            }
-        }
+    } else {
+        bail!("{} rustup was not found. Please, install rustup: https://www.rust-lang.org/tools/install", emoji::ERROR);
     }
-    Ok(())
-}
-
-/// Installs rustup
-fn install_rustup(nightly_version: &str) -> Result<()> {
-    #[cfg(windows)]
-    let rustup_init_path = download_file(
-        "https://win.rustup.rs/x86_64".to_string(),
-        "rustup-init.exe",
-        &get_dist_path("rustup"),
-        false,
-    )?;
-    #[cfg(unix)]
-    let rustup_init_path = download_file(
-        "https://sh.rustup.rs".to_string(),
-        "rustup-init.sh",
-        &get_dist_path("rustup"),
-        false,
-    )?;
-    info!(
-        "{} Installing rustup with {} toolchain",
-        emoji::WRENCH,
-        nightly_version
-    );
-
-    #[cfg(windows)]
-    cmd!(
-        rustup_init_path,
-        "--default-toolchain",
-        nightly_version,
-        "--profile",
-        "minimal",
-        "-y"
-    )
-    .run()?;
-    #[cfg(not(windows))]
-    cmd!(
-        "/bin/bash",
-        rustup_init_path,
-        "--default-toolchain",
-        nightly_version,
-        "--profile",
-        "minimal",
-        "-y"
-    )
-    .run()?;
-
-    #[cfg(windows)]
-    let path = format!(
-        "{};{}",
-        std::env::var("PATH").unwrap(),
-        get_cargo_home().join("bin").display()
-    );
-    #[cfg(unix)]
-    let path = format!(
-        "{}:{}",
-        std::env::var("PATH").unwrap(),
-        get_cargo_home().join("bin").display()
-    );
-
-    std::env::set_var("PATH", path);
-    warn!(
-        "{} Please restart your terminal after the installation for the changes to take effect.",
-        emoji::WARN
-    );
 
     Ok(())
 }
