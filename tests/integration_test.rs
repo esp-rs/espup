dc77c99cf10066943b66169cad61926b91beb6b8
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-08 12:58:30 +0000

    test: 🧪 Update tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -42,27 +42,27 @@ fn verify_uninstall_help() {
         .success();
 }
 
-#[test]
-fn verify_process() {
-    assert_cmd::Command::cargo_bin("espup")
-        .unwrap()
-        .arg("install")
-        .assert()
-        .success();
-    let config_file = espup::config::Config::get_config_path().unwrap();
-    assert!(config_file.exists());
-    assert_cmd::Command::cargo_bin("espup")
-        .unwrap()
-        .arg("update")
-        .assert()
-        .success();
-    let config_file = espup::config::Config::get_config_path().unwrap();
-    assert!(config_file.exists());
-    // assert_cmd::Command::cargo_bin("espup")
-    //     .unwrap()
-    //     .arg("uninstall")
-    //     .assert()
-    //     .success();
-    // let config_file = espup::config::Config::get_config_path().unwrap();
-    // assert!(!config_file.exists());
-}
+// #[test]
+// fn verify_process() {
+//     assert_cmd::Command::cargo_bin("espup")
+//         .unwrap()
+//         .arg("install")
+//         .assert()
+//         .success();
+//     let config_file = espup::config::Config::get_config_path().unwrap();
+//     assert!(config_file.exists());
+//     assert_cmd::Command::cargo_bin("espup")
+//         .unwrap()
+//         .arg("update")
+//         .assert()
+//         .success();
+//     let config_file = espup::config::Config::get_config_path().unwrap();
+//     assert!(config_file.exists());
+//     assert_cmd::Command::cargo_bin("espup")
+//         .unwrap()
+//         .arg("uninstall")
+//         .assert()
+//         .success();
+//     // let config_file = espup::config::Config::get_config_path().unwrap();
+//     // assert!(!config_file.exists());
+// }
