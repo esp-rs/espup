a96700aa8a8cf65701bbf09f6facf27b2a5cbc25
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-08 13:18:18 +0000

    fix: 💚 Fix clippy warnings

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -19,7 +19,7 @@ fn verify_help() {
 fn verify_install_help() {
     assert_cmd::Command::cargo_bin("espup")
         .unwrap()
-        .args(&["install", "--help"])
+        .args(["install", "--help"])
         .assert()
         .success();
 }
@@ -28,7 +28,7 @@ fn verify_install_help() {
 fn verify_update_help() {
     assert_cmd::Command::cargo_bin("espup")
         .unwrap()
-        .args(&["update", "--help"])
+        .args(["update", "--help"])
         .assert()
         .success();
 }
@@ -37,32 +37,7 @@ fn verify_update_help() {
 fn verify_uninstall_help() {
     assert_cmd::Command::cargo_bin("espup")
         .unwrap()
-        .args(&["uninstall", "--help"])
+        .args(["uninstall", "--help"])
         .assert()
         .success();
 }
-
-// #[test]
-// fn verify_process() {
-//     assert_cmd::Command::cargo_bin("espup")
-//         .unwrap()
-//         .arg("install")
-//         .assert()
-//         .success();
-//     let config_file = espup::config::Config::get_config_path().unwrap();
-//     assert!(config_file.exists());
-//     assert_cmd::Command::cargo_bin("espup")
-//         .unwrap()
-//         .arg("update")
-//         .assert()
-//         .success();
-//     let config_file = espup::config::Config::get_config_path().unwrap();
-//     assert!(config_file.exists());
-//     assert_cmd::Command::cargo_bin("espup")
-//         .unwrap()
-//         .arg("uninstall")
-//         .assert()
-//         .success();
-//     // let config_file = espup::config::Config::get_config_path().unwrap();
-//     // assert!(!config_file.exists());
-// }
